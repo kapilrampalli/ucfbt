09b4b799d90d3cdbeedd598e74ae850b2028b4c7
shaanparekh <shaanparekh@utexas.edu>
2022-04-11 15:06:38 -0500

    options

--- ucfbt/assets/option.py
+++ ucfbt/assets/option.py
@@ -1,13 +1,14 @@
+from tokenize import Double
 import pandas as pd
 from ucfbt.assets.asset  import Asset
 
 class Option(Asset):
-    def __init__(self, ticker, prices_df, portfolio):
+    def __init__(self, ticker, prices_df, portfolio, underlying):
         super().__init__(ticker, prices_df, portfolio)
         self.limit_orders = set()
-        self.underlying = ???
-        self.strike = ???
-        self.type = ???
+        self.underlying = underlying
+        self.strike = int(self.ticker[-8:])/1000.0
+        self.type = "Call" if self.ticker[-9] == 'C' else "Put"
     
     def market_buy(self, size):
         self.market_order(size)
@@ -42,11 +43,20 @@ class Option(Asset):
             del self.portfolio[self.ticker]
         while(self.currData("Date") < newTime):
             cur_index = len(fills)
+            if(self.ticker in self.portfolio):
+                if(self.type == "Call" and self.strike <= self.underlying.currData("High")):
+                    self.portfolio["cash"] -= self.portfolio[self.ticker] * 100 * (self.underlying.currData("High") - self.strike)
+                    del self.portfolio[self.ticker]
+                if(self.type == "Put" and self.strike >= self.underlying.currData("Low")):
+                    if self.underlying.ticker not in self.portfolio:
+                        self.portfolio[self.underlying.ticker] = 0
+                    self.portfolio[self.underlying.ticker] += self.portfolio[self.ticker] * 100
+                    self.portfolio["cash"] -= self.portfolio[self.ticker] * 100 * (self.strike)
+                    del self.portfolio[self.ticker]
             for price, size in self.limit_orders:
                 if(self.currData("Low") <= price <= self.currData("High")):
                     self.change_position(price, size)
                     fills.append((price, size))
-                if(self.type == "Call" and self.ti)
             for i in range(cur_index, len(fills)):
                 self.limit_orders.remove(fills[i])
             self.index += 1
@@ -67,6 +77,6 @@ class Option(Asset):
             return 0
     
     def feeModel(self, size):
-        return 0
+        return 0.65 * size
     
         
